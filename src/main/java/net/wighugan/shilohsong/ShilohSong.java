3653260e30a7a27bd64174bb946c58c9b8834ae6
Wighugan <amigingerr@gmail.com>
2025-08-04 22:17:00 +0700

    Initial commit

---
+++ src/main/java/net/wighugan/shilohsong/ShilohSong.java
@@ -0,0 +1,17 @@
+package net.wighugan.shilohsong;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShilohSong implements ModInitializer {
+	public static final String MOD_ID = "shilohsong";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/wighugan/shilohsong/ShilohSongClient.java
@@ -0,0 +1,10 @@
+package net.wighugan.shilohsong;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShilohSongClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
